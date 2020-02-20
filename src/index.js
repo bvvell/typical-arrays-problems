5ab7002bd522607e09b2283e104dcf78a89bdddf
Vladimir Bernacky <vladimir.bernacky@neotech.by>
2020-02-20 15:47:07 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (Array.isArray(array) && array.length) {
+        let min = 0;
+        array.forEach(element => {
+            min = element < min ? element : min
+        })
+        return min;
+    } else {
+        return 0;
+    }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (Array.isArray(array) && array.length) {
+        let max = 0;
+        array.forEach(element => {
+            max = element > max ? element : max
+        })
+        return max;
+    } else {
+        return 0;
+    }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (Array.isArray(array) && array.length) {
+        let summ = 0;
+        array.forEach(Element => {
+            summ += Element
+        })
+        return summ / array.length
+    } else {
+        return 0;
+    }
 }
